490e383450acfdb1861a09bdccd8ec676b121434
24LP2767 24LP2767 <24LP2767@G301-CC0194.local>
2024-12-02 16:39:36 +0800

    info data

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("hello world from")
+
+// data types //
+
+// 1. string => text type => " text "
+// 2. number => number type => 1 6 1000
+// 3. boolean => true or false => false / true
+// 4. object => { key:value }
+// 5. Array => [ index, index ]
+// 6. Null Or Undefined => no value
